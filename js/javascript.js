e8fb35841f4e54fffcd644bac411e260d2a6d10b
Min Naing Oo <minnaingoo792@gmail.com>
2026-04-20 12:15:08 +0630

    dark_mode

---
+++ js/javascript.js
@@ -0,0 +1,26 @@
+// Dark Vs Light Js 
+const light = document.getElementById('light')
+const dark = document.getElementById('dark')
+
+dark.addEventListener('click', ()=>{
+    document.body.classList.add('dark-theme')
+    document.body.classList.remove('light-theme')
+    console.log("Dark Mode On");
+})
+light.addEventListener('click', ()=>{
+    document.body.classList.add('light-theme')
+    document.body.classList.remove('dark-theme')
+    console.log("Light Mode On");
+    
+})
+
+// menu bar js 
+const menutoggle =  document.getElementById('menubar');
+const navlinks = document.querySelector('.navlinks');
+
+
+menutoggle.addEventListener('click' , ()=>{
+    document.body.classList.toggle('show')
+    console.log("Menu Bar Clicked & Toggle");
+    
+})
